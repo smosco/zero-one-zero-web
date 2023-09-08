23e2c2eac721ab1ddcc819acc2a84ef0f4b99cc8
Gyumin Choi <choegyumin@gmail.com>
2023-09-09 00:36:35 +0900

    Reformat code

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config, options) {
     const { dev, isServer } = options;
-    if (dev && !isServer)
-      config.plugins.push(new ForkTsCheckerWebpackPlugin({ devServer: dev }));
+    if (dev && !isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin({ devServer: dev }));
     return config;
   },
 };
