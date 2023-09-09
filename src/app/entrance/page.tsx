0d977ba3707df0562701a9a46b8e2fa0c7dce855
smosco <go1ruf2tk3@gmail.com>
2023-09-10 02:31:10 +0900

    rename: 투표 데이터 변수명 수정

--- src/app/entrance/page.tsx
+++ src/app/entrance/page.tsx
@@ -15,20 +15,23 @@ export default function Entrance() {
   return (
     <div className="max-w-sm mx-auto">
       <div className="flex flex-col gap-16 px-6 py-32">
-        <form onSubmit={submitCode} className="flex flex-col ">
+        <form onSubmit={submitCode} className="flex flex-col gap-8">
           <input
             type="text"
             value={code}
             onChange={(e) => setCode(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400"
+            placeholder="참여 코드를 입력해주세요"
+            className="h-16 mt-1 block w-full px-3 py-2 bg-white border border-slate-300 hover:border-[#8c70d7] rounded-md text-md shadow-sm placeholder-slate-400"
           />
-          <button className="w-full h-16 flex justify-center items-center rounded-md bg-[#eb7a53]">코드 입력</button>
+          <button className="w-full h-16 flex justify-center items-center rounded-md bg-[#8c70d7] text-white">
+            코드 입력
+          </button>
         </form>
         <button
           onClick={() => {
             router.push('/create');
           }}
-          className="w-full h-16 flex justify-center items-center rounded-md bg-[#eb7a53]"
+          className="w-full h-16 flex justify-center items-center rounded-md bg-[#8c70d7] text-white"
         >
           투표 만들러 가기
         </button>
