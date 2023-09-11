b483aacdd81f106d576a8bda1f49f9ab6aea34d4
SMOSCO <62870362+smosco@users.noreply.github.com>
2023-09-11 17:31:11 +0900

    feat: implement voting code input feature (#27)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,61 @@
 'use client';
 
+import { fetchAndGetVote } from '@/apis/api';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 export default function Entrance() {
-  const [code, setCode] = useState<string>('');
-
   const router = useRouter();
+  const [code, setCode] = useState<string>('');
+  const [isError, setIsError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    router.push('/participant');
+  // code를 통해서 voteId 가져오기
+  // voteId를 통해서 해당 voteId의 vote 정보 가져오기
+
+  // code를 통해서 code가 있는지 확인만 하기
+  // code를 통해서 해당 code의 vote 정보 가져오기
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    /**
+     * @todo 투표 코드로 데이터 요청
+     * @todo 데이터 있으면 url에 쿼리로 투표 코드 넘김
+     */
+    fetchAndGetVote().then((data) => {
+      if (data) {
+        router.push(`/participant?${new URLSearchParams({ code: code }).toString()}`);
+      } else {
+        setIsError(true);
+        setErrorMessage('코드가 유효하지 않아요!');
+      }
+    });
   };
 
   return (
     <div className="max-w-sm mx-auto">
       <div className="flex flex-col gap-16 px-6 py-32">
         <form onSubmit={onSubmit} className="flex flex-col gap-8">
-          <input
-            type="text"
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-            placeholder="참여 코드를 입력해주세요"
-            className="h-16 mt-1 block w-full px-3 py-2 bg-white border border-slate-300 hover:border-[#8c70d7] rounded-md text-md shadow-sm placeholder-slate-400"
-          />
-          <button className="w-full h-16 flex justify-center items-center rounded-md bg-[#8c70d7] text-white">
+          <div className="flex flex-col gap-2">
+            <input
+              type="text"
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              placeholder="참여 코드를 입력해주세요"
+              required
+              className="h-16 mt-1 block w-full px-3 py-2 bg-white border-2 border-indigo-200 outline-none focus:border-indigo-400 rounded-lg text-md placeholder-gray-400"
+            />
+            {true && <p className="text-gray-200 font-md">{errorMessage}</p>}
+          </div>
+          <button className="w-full h-16 flex justify-center items-center rounded-md bg-indigo-500 text-white">
             코드 입력
           </button>
         </form>
         <button
           onClick={() => {
-            router.push('/make');
+            router.push('/create');
           }}
-          className="w-full h-16 flex justify-center items-center rounded-md bg-[#8c70d7] text-white"
+          className="w-full h-16 flex justify-center items-center rounded-md bg-indigo-500 text-white"
         >
           투표 만들러 가기
         </button>
--- src/app/participant/page.tsx
+++ src/app/participant/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import axios from 'axios';
+import { fetchAndGetVote } from '@/apis/api';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 interface iParticipantList {
@@ -22,49 +23,45 @@ interface iVoteType {
 
 export default function Participant() {
   const router = useRouter();
-  const [selectedUser, setSelectedUser] = useState('');
+  const search = useSearchParams();
+  const code = search.get('code');
+  const [selectedUserName, setSelectedUserName] = useState('');
   const [vote, setVote] = useState<iVoteType>();
 
-  const getVote = async () => {
-    try {
-      /** @todo API 통합 */
-      const { data } = await axios.get('/dummy/vote.json');
-      return data;
-    } catch (err) {
-      /** @todo 에러 핸들링 */
-      console.log('get vote person error : ', err);
-    }
-  };
-
   useEffect(() => {
-    getVote().then((data) => setVote(data));
+    fetchAndGetVote().then((data) => setVote(data));
   }, []);
 
-  const hasSelected: boolean = selectedUser !== '';
-
-  const isCompleted: iParticipantList | undefined = vote?.participantList.find((item) => {
-    return item.participantName === selectedUser;
-  });
+  const hasSelectedUserName: boolean = selectedUserName !== '';
+  const isCompleted: boolean | undefined = vote?.participantList.find((item) => {
+    return item.participantName === selectedUserName;
+  })?.isSelected;
+  const numOfParticipant: number | undefined = vote?.participantList.length;
+  const participantMoreThanSix: boolean | 0 | undefined = numOfParticipant && numOfParticipant > 6;
 
   const changeUser = (userName: string) => {
-    if (selectedUser === userName) {
-      setSelectedUser('');
+    if (selectedUserName === userName) {
+      setSelectedUserName('');
     } else {
-      setSelectedUser(userName);
+      setSelectedUserName(userName);
     }
   };
 
   return (
     <div className="max-w-sm mx-auto">
-      <div className="flex flex-col gap-16 px-6 py-32">
+      <div className="flex flex-col gap-16 px-6 pt-24 pb-8">
         <h1 className="w-full text-center text-xl text-black font-bold">{vote?.voteTitle}</h1>
-        <ul className="w-full grid grid-cols-2 gap-4">
+        <ul
+          className={`h-[30rem] w-full grid ${
+            participantMoreThanSix ? 'grid-cols-3' : 'grid-cols-2'
+          } gap-x-4 gap-y-6 overflow-y-scroll`}
+        >
           {vote?.participantList.map(({ participantId, participantName, isSelected }) => (
             <li
               key={participantId}
               onClick={() => changeUser(participantName)}
-              className={`h-[140px] flex justify-center items-center relative rounded-lg overflow-hidden ${
-                selectedUser === participantName ? 'bg-[#e4f18b]' : 'bg-[#f0f2f5]'
+              className={`h-[140px] flex justify-center items-center relative rounded-lg overflow-hidden bg-indigo-50 border-2 border-solid cursor-pointer ${
+                selectedUserName === participantName ? 'border-indigo-200' : 'border-gray-100'
               }`}
             >
               <p className="text-lg font-bold">{participantName}</p>
@@ -82,13 +79,13 @@ export default function Participant() {
         </ul>
         {vote && (
           <button
-            disabled={!hasSelected}
-            onClick={() => router.push(isCompleted?.isSelected ? '/result' : '/vote')}
-            className={`w-full h-16 flex justify-center items-center rounded-md bg-[#8c70d7] text-white ${
-              hasSelected ? 'opacity-100' : 'opacity-70'
+            disabled={!hasSelectedUserName}
+            onClick={() => router.push(isCompleted ? '/result' : '/vote')}
+            className={`w-full h-16 flex justify-center items-center rounded-md bg-indigo-500 text-white ${
+              hasSelectedUserName ? 'opacity-100' : 'opacity-60'
             }`}
           >
-            {isCompleted?.isSelected ? '투표결과 보러가기' : '투표하러 가기'}
+            {isCompleted ? '투표결과 보러가기' : '투표하러 가기'}
           </button>
         )}
       </div>
